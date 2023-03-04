4a51dbb68f1e989fa8f05aea50d722edc018222b
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2023-03-04 11:23:40 +0100

    Implement App component and set up React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Navbar from './components/Navbar';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: '#000' }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<h1>Feed</h1>} />
+          <Route path="/video/:id" element={<h1>VideoDetail</h1>} />
+          <Route path="/channel/:id" element={<h1>ChannelDetail</h1>} />
+          <Route path="/search/:searchTerm" element={<h1>SearchFeed</h1>} />
+        </Routes>
+      </Box>
+    </BrowserRouter>
+  );
 };
 
 export default App;
