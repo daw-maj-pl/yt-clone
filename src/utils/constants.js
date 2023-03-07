1d5f557406c1818b74f332da89584fc8ebeb0087
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2023-03-07 18:44:50 +0100

    Create list of categories

---
+++ src/utils/constants.js
@@ -0,0 +1,35 @@
+import HomeIcon from '@mui/icons-material/Home';
+import TerminalIcon from '@mui/icons-material/Terminal';
+import HtmlIcon from '@mui/icons-material/Html';
+import CssIcon from '@mui/icons-material/Css';
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import SchoolIcon from '@mui/icons-material/School';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SportsTennisIcon from '@mui/icons-material/SportsTennis';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon /> },
+  { name: 'Coding', icon: <TerminalIcon /> },
+  { name: 'HTML', icon: <HtmlIcon /> },
+  { name: 'CSS', icon: <CssIcon /> },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon /> },
+  { name: 'Podcast', icon: <GraphicEqIcon /> },
+  { name: 'Movie', icon: <OndemandVideoIcon /> },
+  { name: 'Gaming', icon: <SportsEsportsIcon /> },
+  { name: 'Live', icon: <LiveTvIcon /> },
+  { name: 'Sport', icon: <SportsTennisIcon /> },
+  { name: 'Fashion', icon: <CheckroomIcon /> },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon /> },
+  { name: 'Comedy', icon: <TheaterComedyIcon /> },
+  { name: 'Gym', icon: <FitnessCenterIcon /> },
+  { name: 'Crypto', icon: <DeveloperModeIcon /> }
+];
