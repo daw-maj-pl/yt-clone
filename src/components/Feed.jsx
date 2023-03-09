01d7f797d55494529a0c84313f02e39a706c6b4c
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2023-03-09 20:10:11 +0100

    Fetch videos and render Videos component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 import Sidebar from './Sidebar';
+import Videos from './Videos';
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState(null);
+
+  useEffect(() => {
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then(data =>
+      setVideos(data.items)
+    );
+  }, [selectedCategory]);
 
   return (
     <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
@@ -26,6 +37,19 @@ const Feed = () => {
         >
           Copyright © 2023 DM
         </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: 'white' }}
+        >
+          {selectedCategory} <span style={{ color: '#FC1503' }}>videos</span>
+        </Typography>
+
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
