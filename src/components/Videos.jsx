d8c6a36042b50b3cbad7ff4ca7cbdcb1025fe2d2
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2023-03-13 20:20:31 +0100

    Create ChannelCard component

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,5 +1,6 @@
 import { Stack, Box } from '@mui/material';
 import VideoCard from './VideoCard';
+import ChannelCard from './ChannelCard';
 
 const Videos = ({ videos }) => {
   if (!videos?.length) return '<Loader />;';
@@ -12,12 +13,15 @@ const Videos = ({ videos }) => {
       alignItems="start"
       gap={2}
     >
-      {videos.map((item, idx) => (
-        <Box key={idx}>
-          {item.id.videoId && <VideoCard video={item} />}
-          {item.id.channelId && '<ChannelCard />'}
-        </Box>
-      ))}
+      {videos.map(
+        (item, idx) =>
+          (item?.id?.videoId || item?.id?.channelId) && (
+            <Box key={idx}>
+              {item.id.videoId && <VideoCard video={item} />}
+              {item.id.channelId && <ChannelCard channelDetail={item} />}
+            </Box>
+          )
+      )}
     </Stack>
   );
 };
