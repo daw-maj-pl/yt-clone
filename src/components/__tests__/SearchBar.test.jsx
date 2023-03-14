e83a19fdc8d63e5963d23467628442b39d444088
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2023-03-14 18:10:04 +0100

    Update tests for SearchBar to fix: "useNavigate() may be used only in the context of a <Router> component"

--- src/components/__tests__/SearchBar.test.jsx
+++ src/components/__tests__/SearchBar.test.jsx
@@ -1,14 +1,15 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import SearchBar from '../SearchBar';
 
 test('renders search input', () => {
-  render(<SearchBar />);
+  render(<SearchBar />, { wrapper: BrowserRouter });
   const searchInputEl = screen.getByRole('textbox');
   expect(searchInputEl).toBeInTheDocument();
 });
 
 test('renders button of type submit', () => {
-  render(<SearchBar />);
+  render(<SearchBar />, { wrapper: BrowserRouter });
   const searchBtnEl = screen.getByRole('button', {
     name: /search/i
   });
