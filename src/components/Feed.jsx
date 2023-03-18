d39fec50406e2e7e6e481e8e304866edb8682825
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2023-03-18 14:15:10 +0100

    Refactor: Create cleanup function

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -9,11 +9,11 @@ const Feed = () => {
   const [videos, setVideos] = useState(null);
 
   useEffect(() => {
-    setVideos(null);
-
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then(data =>
       setVideos(data.items)
     );
+
+    return () => setVideos(null);
   }, [selectedCategory]);
 
   return (
